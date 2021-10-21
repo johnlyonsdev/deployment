cef9be6d2ddfee47f02e26411ff4c51393c17ac1
John Lyons <johnlyonsdev@gmail.com>
2021-10-21 17:14:42 -0500

    Added final version

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 let background = document.querySelector("div")
+let buyNow = document.querySelector('h2')
+let store = document.querySelector('.store')
+
 background.addEventListener("mouseover", () => {
 	background.classList.add('hover')
+    buyNow.classList.add('hover')
 });
 
 background.addEventListener("mouseout", () => {
 	background.classList.remove('hover')
+    buyNow.classList.remove('hover')
 });
 
-let buyNow = document.querySelector('h2')
-
-background.addEventListener("mouseover", () => {
-	buyNow.classList.add('hover')
+store.addEventListener("mouseover", () => {
+	store.classList.add('hover')
 });
 
-background.addEventListener("mouseout", () => {
-	buyNow.classList.remove('hover')
+store.addEventListener("mouseout", () => {
+	store.classList.remove('hover')
 });
