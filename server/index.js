14cfd6b3311b625aa112a32b5cdc4646d7c430a5
John Lyons <johnlyonsdev@gmail.com>
2021-10-21 15:33:22 -0500

    undo

--- server/index.js
+++ server/index.js
@@ -2,11 +2,10 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-
-app.get('/js', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "../index.html"))
 })
 
 const port = process.env.PORT || 4000
 
-app.listen(port, () => console.log(`Take us to warp ${port}`))
+app.listen(port, () => console.log(`Take us to warp ${port}`)) 
