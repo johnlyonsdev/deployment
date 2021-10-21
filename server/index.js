cee7c074440f64398257b01211f022affb2029c7
John Lyons <johnlyonsdev@gmail.com>
2021-10-21 15:22:23 -0500

    6.1

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-app.get('/js', function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.html"))
-})
-app.get('/css', function(req, res) {
-    res.sendFile(path.join(__dirname, "styles.css"))
-})
+// app.get('/js', function(req, res) {
+//     res.sendFile(path.join(__dirname, "../index.html"))
+// })
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, "styles.css"))
+// })
+
+app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+
 const port = process.env.PORT || 4000
 
 app.listen(port, () => console.log(`Take us to warp ${port}`))
