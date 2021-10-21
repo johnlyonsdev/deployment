135c8b4a55a21038a9d3cdbe361d16c3cbc63152
John Lyons <johnlyonsdev@gmail.com>
2021-10-21 15:00:14 -0500

    attempting css fix 4.0

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const path = require('path')
 const app = express()
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "../index.html"))
-    res.sendFile(path.join(__dirname, "..styles.css"))
-
 })
-
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "..styles.css"))
+})
 const port = process.env.PORT || 4000
 
 app.listen(port, () => console.log(`Take us to warp ${port}`))
