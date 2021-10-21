5d420b86a6e7044d99a9cd9902329c58fe65c87b
John Lyons <johnlyonsdev@gmail.com>
2021-10-21 15:08:08 -0500

    6.0

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-app.get('/', function(req, res) {
+app.get('/js', function(req, res) {
     res.sendFile(path.join(__dirname, "../index.html"))
 })
-app.get('/', function(req, res) {
+app.get('/css', function(req, res) {
     res.sendFile(path.join(__dirname, "styles.css"))
 })
 const port = process.env.PORT || 4000
