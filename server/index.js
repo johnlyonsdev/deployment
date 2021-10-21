72946efa32b0c410917107a64ea14efe680f154d
John Lyons <johnlyonsdev@gmail.com>
2021-10-21 16:02:53 -0500

    testing

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const app = express()
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "../index.html"))
 })
-
+app.get('/css', function(req, res) {
+    res.sendFile(path.join(__dirname, "../styles.css"))
+})
 const port = process.env.PORT || 4000
 
 app.listen(port, () => console.log(`Take us to warp ${port}`)) 
