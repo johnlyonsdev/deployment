2a4b7db1143a849cc768c976d901c788d54bfffb
John Lyons <johnlyonsdev@gmail.com>
2021-10-21 14:56:02 -0500

    attempting css fix 3.0

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const app = express()
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "../index.html"))
+    res.sendFile(path.join(__dirname, "..styles.css"))
 
 })
 
